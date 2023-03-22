b8eb973eb8a1936d9f46bce407120cb1b2e2fd33
Ruben Antonio Moreno Postigo <moreno.greenwater@gmail.com>
2023-03-22 15:35:42 -0500

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { FiltrosComponent } from './filtros/filtros.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    FiltrosComponent,
+    PageWrapperComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
